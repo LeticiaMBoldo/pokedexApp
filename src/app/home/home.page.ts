0d23e7c03734947667a9813d15f2e74e1a72645d
LeticiaMBoldo <60079653+LeticiaMBoldo@users.noreply.github.com>
2020-02-17 20:25:57 -0300

    Lista de pokemons por codigo
    Ajusta a interface para percorrer uma lista dinamica de pokemons, ajustar as cores de eletric e water

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+  public listaPokemons = [
+    {numero : '001',
+     nome: 'Bulbasaur',
+     tipos: ['Grass', 'Poison'],
+     img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+    },
+    {numero:'002',
+     nome: 'Ivysaur',
+     tipos: ['Grass', 'Poison'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
+    },
+    {numero:'003',
+     nome: 'Venusaur',
+     tipos: ['Grass', 'Poison'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
+    },
+    {numero:'004',
+     nome: 'Charmander',
+     tipos: ['Fire'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+    },
+    {numero:'005',
+     nome: 'Charmeleon',
+     tipos: ['Fire'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png"
+    },
+    {numero:'006',
+     nome: 'Charizard',
+     tipos: ['Fire', 'Flying'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"
+    },
+    {numero:'025',
+     nome: 'Pikachu',
+     tipos: ['Eletric'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
+    },
+    {numero:'026',
+     nome: 'Raichu',
+     tipos: ['Eletric'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png"
+    },
+    {numero:'007',
+     nome: 'Squirtle',
+     tipos: ['Water'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
+    },
+    {numero:'008',
+     nome: 'Wartortle',
+     tipos: ['Water'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png"
+    },
+    {numero:'009',
+     nome: 'Blastoise',
+     tipos: ['Water'],
+     img : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"
+    }
+
+
+  ];
+  public listaFiltrada = [];
+
   constructor() {}
 
 }
